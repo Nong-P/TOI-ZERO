ffd5239364b0bffd47cb376576aaaa72b98f6be8
NongP <kunlaithipsuda@gmail.com>
2026-04-22 14:27:19 +0700

    Day 4.2

--- dowload.py
+++ dowload.py
@@ -4,7 +4,7 @@ import os
 base_url = "https://toi-coding.informatics.buu.ac.th/00-pre-toi/tasks/{}/statements/TH"
 
 headers = {
-    "Cookie": "00-pre-toi_login=2|1:0|10:1776580215|16:00-pre-toi_login|144:WyI2OXRvaXplcm8wODkwMiIsICJiY3J5cHQ6JDJhJDEwJFY3Qzc4emJ4TFl6M1NPdjlIVGEzUE9xTEFRWElTcTE4WG01MEFxZkhGak8xdFV0VTJWemlHIiwgMTc3NjU4MDIxNS41ODI1MjJd|255320df39c3d89992b87c8b36ac1bbdade32d131c30290b6ff3cfbd192fd43b; _xsrf=2|c9ade5e1|fc47e9d13a7453c5f20c59e37ca735dc|1776570784"
+    "Cookie": ""
 }
 
 tasks = []
